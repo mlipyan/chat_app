d006b98e020de1c74a470bfa84002fae04b2a65a
Max L <mlipyan@gmail.com>
2020-04-26 10:06:28 -0400

    added user txt

--- chat.js
+++ chat.js
@@ -8,14 +8,14 @@ var loc = 0;
 context.fillStyle = "black";
 context.fillRect(0, 0, canvas_size, canvas_size);
 
-function draw(msg, loc){
+function draw(msg, loc, color){
     context.font = "20px Arial";
-    context.fillStyle = "white";
+    context.fillStyle = color;
     context.fillText(msg, 50, 50 + loc);
 }
 
 const name = prompt('name: ');
-draw('Hi, ' + name + '. Press \'m\' to type.', loc);
+draw('Hi, ' + name + '. Press \'m\' to type.', loc, 'white');
 socket.emit('init', name);
 
 function keyRespond(evt){
@@ -29,7 +29,9 @@ window.addEventListener('keydown', keyRespond);
 loc += 30;
 
 socket.on('message', function(data) {
-    
-    if (data['name'] != name) {draw(data['msg'], loc); loc += 30;};
+    let msg = name + ': ' + data['msg'];
+    if (data['name'] != name) {draw(msg, loc, 'green'); loc += 30;};
+    if (data['name'] === name) {draw(msg, loc, 'pink'); loc += 30;};
+
 });
 
